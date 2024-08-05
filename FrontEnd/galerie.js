7bab79ff41fe24379644862628cf8598bed63e72
Manon bouquin <Manon.bouquin@hotmail.fr>
2024-08-05 09:23:53 +0200

    modification des fichiers

--- FrontEnd/galerie.js
+++ FrontEnd/galerie.js
@@ -1,55 +1,52 @@
 //Récupération API
 async function fetchProjets() {
-  const response = await fetch('http://localhost:5678/api/works');
-  return await response.json();
-};
+  const response = await fetch('http://localhost:5678/api/works')
+  return await response.json()
+}
 // Fonction afficher projets
 function afficherGalerie(projets) {
-  const galerie = document.querySelector(".galerie");
-  galerie.innerHTML = "";
+  const galerie = document.querySelector(".galerie")
+  galerie.innerHTML = ""
   projets.forEach(projet => {
-    const articleElement = document.createElement("article");
-    const imageElement = document.createElement("img");
-    imageElement.src = projet.imageUrl;
-    imageElement.alt = projet.title;
-    const nomElement = document.createElement("h3");
-    nomElement.innerText = projet.title;
-    const categorieElement = document.createElement("p");
-    categorieElement.innerText = projet.category;
-    articleElement.classList.add("projet");
-    articleElement.appendChild(imageElement);
-    articleElement.appendChild(nomElement);
-    articleElement.appendChild(categorieElement);
-
-    galerie.appendChild(articleElement);
-  });
+    const articleElement = document.createElement("article")
+    const imageElement = document.createElement("img")
+    imageElement.src = projet.imageUrl
+    imageElement.alt = projet.title
+    const nomElement = document.createElement("h3")
+    nomElement.innerText = projet.title
+    
+    articleElement.classList.add("projet")
+    articleElement.appendChild(imageElement)
+    articleElement.appendChild(nomElement)
+    
+    galerie.appendChild(articleElement)
+  })
 }
 document.addEventListener("DOMContentLoaded", async () => {
-  const btnCategories = document.querySelector(".categorie");
+  const btnCategories = document.querySelector(".categorie")
   const categories = [
     { id: 0, name: "Tous" },
     { id: 1, name: "Objets" },
     { id: 2, name: "Appartements" },
     { id: 3, name: "Hotels & Restaurants" }
- ];
+ ]
   // Fonction pour filtrer et afficher les projets
   function filtrerGalerie(projets, categoryId) {
-    const projetsFiltrees = categoryId === 0 ? projets : projets.filter(projet => projet.categoryId === categoryId);
-    afficherGalerie(projetsFiltrees);
+    const projetsFiltrees = categoryId === 0 ? projets : projets.filter(projet => projet.categoryId === categoryId)
+    afficherGalerie(projetsFiltrees)
   }
   // BOUTONS
-  const projets = await fetchProjets();
+  const projets = await fetchProjets()
   categories.forEach(category => {
-    const bouton = document.createElement("button");
-    bouton.type = "button";
-    bouton.textContent = category.name;
+    const bouton = document.createElement("button")
+    bouton.type = "button"
+    bouton.textContent = category.name
     bouton.addEventListener("click", () => {
-      console.log(`Le bouton ${category.name} a été cliqué !`);
-      filtrerGalerie(projets, category.id);
-    });
-    btnCategories.appendChild(bouton);
-  });
-  afficherGalerie(projets);
-});
+      filtrerGalerie(projets, category.id)
+    })
+    btnCategories.appendChild(bouton)
+  })
+  afficherGalerie(projets)
+})
 
 
---
+++ FrontEnd/login.js
@@ -0,0 +1,42 @@
+const logInForm = document.getElementById("form-connexion")
+logInForm.addEventListener ("submit", (event) => {
+    event.preventDefault()
+    const email = document.getElementById("email").value.trim() //Recup valeur entrées par l'utilisateur
+    const password = document.getElementById("pass").value.trim()
+    const logInElement = {
+        email: email,
+        password: password,
+    }
+    logIn(logInElement)
+})
+async function logIn(logInElement) {
+
+        const chargeUtile = JSON.stringify(logInElement)
+        const reponse = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {"Content-Type":"application/json"},
+            body: chargeUtile
+        })
+
+        if (reponse.ok) {
+            const data = await reponse.json();
+            window.localStorage.setItem("token", data.token);
+            window.location.replace("index.html");
+        }else {
+            afficherErreur(reponse)
+        }
+}
+function afficherErreur (reponse) {
+    let errorMessage
+   switch (reponse.status) {
+    case 401: 
+    case 404:
+        errorMessage = "L'email ou le mot de passe n'est pas correct."
+        break
+   }
+   let popupErreur = document.querySelector(".connexion-erreur")
+   popupErreur.textContent = errorMessage
+   popupErreur.classList.add("active")
+}
+
+
